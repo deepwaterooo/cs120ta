6d03d846bbd997fc5523d7d3e077916d1232830c
deepwaterooo <heyanh@vandals.uidaho.edu>
2014-11-18 18:33:26 -0800

    organize

---
+++ lab12/pointer.cpp
@@ -0,0 +1,23 @@
+// address dereference
+// to help understand pointers slightly better
+// draw on board layout
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nNumber;
+    int *pPointer;
+
+    nNumber = 15;
+    pPointer = &nNumber;
+
+    cout << "address of nNumber is equal to :" << (&nNumber) <<endl;
+    cout << "the value of pPointer is:       " << pPointer << endl;
+    cout << "the value of address where pPointer points to is " << (*pPointer) << endl;
+    
+    *pPointer = 25;
+    cout<<"nNumber is equal to :"<< nNumber <<endl;
+
+    return 0;
+}
---
+++ lab12/robot.h
@@ -0,0 +1,78 @@
+#incluxde <iostream>
+using namespace std;
+
+class robot
+{
+public:
+    robot(int);
+    void refresh(){moved = 0;};
+    void draw();
+    void print();
+    void move(int &, int &);
+private:
+    int direction;
+    int energy;
+    int id;
+    int moved;
+    void turnLeft();
+    void turnRight();
+    void forward(int &x, int &y);
+};
+
+robot::robot(int x) {
+    energy = 50;
+    id = x;
+    moved = 0;
+    direction = 0;
+}
+
+void robot::draw() {
+    cout << "#";
+}
+
+void robot::print() {
+    cout << "id " << id << ": Energy = " << energy << "  direction = " << direction;
+}
+
+void robot::move(int &x, int &y) {
+    if (moved == 1) {
+        return;
+    }
+    switch (rand() % 4) {
+    case 0:
+        turnLeft();
+        break;
+    case 1:
+        turnRight();
+        break;
+    case 2:
+    case 3:
+        forward(x,y);
+        break;
+    default:
+        cout << "Error in robot move." << endl;
+    }
+    moved = 1;
+}
+
+void robot::turnLeft() {
+    energy--;
+    direction = (direction + 3) % 4;
+}
+
+void robot::turnRight() {
+    energy--;
+    direction = (direction + 1) % 4;
+}
+
+void robot::forward(int &x, int &y) {
+    energy -= 2;
+    if (direction == 0)
+        y--;
+    if (direction == 2)
+        y++;
+    if (direction == 1)
+        x++;
+    if (direction == 3)
+        x--;
+}
